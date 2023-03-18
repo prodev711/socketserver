39b0c69763fee829eedf13ac84b74c70742e8ee9
DigitalAgency711 <roman@directnorth.digital>
2023-03-19 00:26:21 +0200

    test

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ mongoose.connect(`mongodb+srv://${process.env.mongousername}:${process.env.passw
 const db=mongoose.connection;
 db.on("error",console.error.bind(console,"connection error : "));
 db.once("open",function(){
-    console.log("Connected successfully !!!");
+    console.log("Connected successfully !!");
 });
 
 /*********************************    End MongoDB connection     *************************/
