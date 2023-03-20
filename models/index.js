e18e93eecab1d965d6e3e7c7fc081d5d84185a55
DigitalAgency711 <roman@directnorth.digital>
2023-03-20 19:56:59 +0200

    adsf

--- models/index.js
+++
@@ -1,6 +0,0 @@
-const User = require('./User');
-const Vulve = require('./Vulve');
-module.exports = {
-    User,
-    Vulve
-}
--- services/index.js
+++
@@ -1,7 +0,0 @@
-const vulveService = require("./vulve");
-const userService = require("./user");
-
-module.exports = {
-    vulveService,
-    userService
-}
